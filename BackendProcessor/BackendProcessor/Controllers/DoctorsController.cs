5e02b7e50c07050787828ae6931aef148b96d4a6
superalex-dev <mr.naidobrixjr@gmail.com>
2023-10-01 19:49:58 +0300

    Created DoctorsController

---
+++ BackendProcessor/BackendProcessor/Controllers/DoctorsController.cs
@@ -0,0 +1,77 @@
+﻿using BackendProcessor.Models;
+using BackendProcessor.Repositories;
+using BackendProcessor.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendProcessor.Controllers
+{
+    public class DoctorsController : ControllerBase
+    {
+        private readonly IDoctorRepository _doctorRepository;
+
+        public DoctorsController(IDoctorRepository doctorRepository)
+        {
+            _doctorRepository = doctorRepository;
+        }
+
+        [HttpGet("doctors/get")]
+        public async Task<IActionResult> GetDoctorsAsync()
+        {
+            IEnumerable<Doctor> doctors = await _doctorRepository.GetAllDoctors();
+
+            if (doctors == null || doctors.Count() == 0)
+            {
+                return NoContent();
+            }
+
+            return Ok(doctors);
+        }
+
+        [HttpGet("doctors/get/{id}")]
+        public async Task<IActionResult> GetDoctorAsync(int doctorId)
+        {
+            Doctor doctor = await _doctorRepository.GetDoctor(doctorId);
+
+            if (doctor == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(doctor);
+        }
+
+        [HttpPost("doctors/add")]
+        public async Task<IActionResult> AddDoctorAsync([FromBody] Doctor doctor)
+        {
+            if (doctor == null)
+            {
+                return BadRequest();
+            }
+
+            await _doctorRepository.AddDoctor(doctor);
+
+            return CreatedAtAction(nameof(GetDoctorAsync), new { doctorId = doctor.DoctorId }, doctor);
+        }
+
+        [HttpPut("doctors/edit/{id}")]
+        public async Task<IActionResult> EditDoctorAsync(int doctorId, [FromBody] Doctor doctor)
+        {
+            if (doctor == null || doctorId != doctor.DoctorId)
+            {
+                return BadRequest();
+            }
+
+            await _doctorRepository.UpdateDoctor(doctor);
+
+            return NoContent();
+        }
+
+        [HttpDelete("doctors/delete/{id}")]
+        public async Task<IActionResult> DeleteDoctorAsync(int doctorId)
+        {
+            await _doctorRepository.DeleteDoctor(doctorId);
+
+            return NoContent();
+        }
+    }
+}
