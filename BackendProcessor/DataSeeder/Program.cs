920dbe41c1b8c0184a7affd44a3f36b368297f9f
superalex-dev <mr.naidobrixjr@gmail.com>
2023-10-01 14:29:28 +0300

    Project created for data seeding.

---
+++ BackendProcessor/DataSeeder/Program.cs
@@ -0,0 +1,14 @@
+﻿using BackendProcessor.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<HospitalDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=HealthEdgeDB;Integrated Security=True;");
+        }
+    }
+}
