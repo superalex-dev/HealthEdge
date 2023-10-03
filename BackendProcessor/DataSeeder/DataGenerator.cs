a3cc4f033ddf00afbc65fbf916815512805d05ce
superalex-dev <mr.naidobrixjr@gmail.com>
2023-10-03 21:57:55 +0300

    Turn on DataGenerator again, program.cs in project DataSeeder is with old connection string

--- BackendProcessor/DataSeeder/DataGenerator.cs
+++ BackendProcessor/DataSeeder/DataGenerator.cs
@@ -10,30 +10,30 @@ namespace DataSeeder
 {
     public static class DataGenerator
     {
-        //public static List<Doctor> GenerateDoctors(int count)
-        //{
-        //    var doctorFaker = new Faker<Doctor>()
-        //        .RuleFor(d => d.FirstName, f => f.Name.FirstName())
-        //        .RuleFor(d => d.LastName, f => f.Name.LastName())
-        //        .RuleFor(d => d.Specialization, f => f.Commerce.Department())
-        //        .RuleFor(p => p.ContactNumber, f => f.Phone.PhoneNumber())
-        //        .RuleFor(d => d.Email, f => f.Internet.Email());
+        public static List<Doctor> GenerateDoctors(int count)
+        {
+            var doctorFaker = new Faker<Doctor>()
+                .RuleFor(d => d.FirstName, f => f.Name.FirstName())
+                .RuleFor(d => d.LastName, f => f.Name.LastName())
+                .RuleFor(d => d.Specialization, f => f.Commerce.Department())
+                .RuleFor(p => p.ContactNumber, f => f.Phone.PhoneNumber())
+                .RuleFor(d => d.Email, f => f.Internet.Email());
 
-        //    return doctorFaker.Generate(count);
-        //}
+            return doctorFaker.Generate(count);
+        }
 
-        //public static List<Patient> GeneratePatients(int count)
-        //{
-        //    var patientFaker = new Faker<Patient>()
-        //        .RuleFor(p => p.FirstName, f => f.Name.FirstName())
-        //        .RuleFor(p => p.LastName, f => f.Name.LastName())
-        //        .RuleFor(p => p.DateOfBirth, f => f.Date.Past(80))
-        //        .RuleFor(p => p.Gender, f => f.PickRandom(new[] { "M", "F" }))
-        //        .RuleFor(p => p.ContactNumber, f => f.Phone.PhoneNumber())
-        //        .RuleFor(p => p.Address, f => f.Address.FullAddress());
+        public static List<Patient> GeneratePatients(int count)
+        {
+            var patientFaker = new Faker<Patient>()
+                .RuleFor(p => p.FirstName, f => f.Name.FirstName())
+                .RuleFor(p => p.LastName, f => f.Name.LastName())
+                .RuleFor(p => p.DateOfBirth, f => f.Date.Past(80))
+                .RuleFor(p => p.Gender, f => f.PickRandom(new[] { "M", "F" }))
+                .RuleFor(p => p.ContactNumber, f => f.Phone.PhoneNumber())
+                .RuleFor(p => p.Address, f => f.Address.FullAddress());
 
-        //    return patientFaker.Generate(count);
-        //}
+            return patientFaker.Generate(count);
+        }
 
         //public static List<Room> GenerateRooms(int count)
         //{
