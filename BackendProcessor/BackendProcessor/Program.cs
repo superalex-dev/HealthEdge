29fae385e8732af358fc8f728c48421bff671a84
Alexander Boev <naidobrixxjr@gmail.com>
2023-12-01 11:53:06 +0200

    remove unnecessary code

--- BackendProcessor/BackendProcessor/Program.cs
+++ BackendProcessor/BackendProcessor/Program.cs
@@ -8,7 +8,6 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IPatientRepository, PatientRepository>();
 builder.Services.AddScoped<IDoctorRepository, DoctorRepository>();
@@ -33,14 +32,12 @@ builder.Services.AddAuthentication(opt => {
     });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<HospitalDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("HospitalDbContext")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
