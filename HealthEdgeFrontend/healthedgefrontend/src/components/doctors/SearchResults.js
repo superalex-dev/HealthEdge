51e365ff83dd09e6639aed8612a78fcef73da72b
Alexander Boev <naidobrixxjr@gmail.com>
2024-04-22 10:45:25 +0300

    continue working on problem with fetching in component filtered doctors

--- HealthEdgeFrontend/healthedgefrontend/src/components/doctors/SearchResults.js
+++ HealthEdgeFrontend/healthedgefrontend/src/components/doctors/SearchResults.js
@@ -6,30 +6,58 @@ const SearchResults = () => {
   const [doctors, setDoctors] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
+  const [specializations, setSpecializations] = useState([]);
+  const [regions, setRegions] = useState([]);
   const location = useLocation();
+
+  useEffect(() => {
+    const fetchSpecializationsAndRegions = async () => {
+      try {
+        const [specializationsResponse, regionsResponse] = await Promise.all([
+          axios.get('http://localhost:5239/specializations'),
+          axios.get('http://localhost:5239/regions')
+        ]);
+        setSpecializations(specializationsResponse.data);
+        setRegions(regionsResponse.data);
+      } catch (error) {
+        console.error('Error fetching specializations and regions:', error);
+      }
+    };
+
+    fetchSpecializationsAndRegions();
+  }, []);
 
   const fetchDoctors = async (search) => {
     setLoading(true);
-    const { specialization, city } = search;
+    const { specialization, city, name = "" } = search;
+    const selectedSpecialization = specializations.find(s => s.name === specialization);
+    const selectedCity = regions.find(c => c.name === city);
+    if (!selectedSpecialization || !selectedCity) {
+      console.error('Invalid search parameters:', search);
+      setError('Invalid search parameters. Please try again.');
+      setLoading(false);
+      return;
+    }
     try {
-      const response = await axios.get('http://localhost:5239/doctors/search', {
-        params: {
-          specializationId: specialization,
-          regionId: city
-        }
-      });
+      let params = {
+        specializationId: selectedSpecialization.id,
+        regionId: selectedCity.id
+      };
+      if (name) {
+        params.name = name;
+      }
+      const response = await axios.get('http://localhost:5239/doctors/search', { params });
       setDoctors(response.data);
     } catch (error) {
       console.error('Error fetching data:', error);
       setError('Failed to fetch data. Please try again later.');
     }
     setLoading(false);
-  };  
+  };
 
   useEffect(() => {
     if (location.state) {
-      const search = location.state.search;
-      fetchDoctors(search);
+      fetchDoctors(location.state.search);
     }
   }, [location.state]);
 
