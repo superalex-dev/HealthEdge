a7e24a83fafa925d8ea9c5448f35d06b111e6cb5
superalex-dev <mr.naidobrixjr@gmail.com>
2024-04-29 09:32:05 +0300

    make Status Appointment column nullable

--- BackendProcessor/BackendProcessor/Migrations/20240429063035_StatusAppointmentColumnMakeNullable.Designer.cs
+++ BackendProcessor/BackendProcessor/Migrations/20240429063035_StatusAppointmentColumnMakeNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BackendProcessor.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BackendProcessor.Migrations
 {
     [DbContext(typeof(HospitalDbContext))]
-    partial class HospitalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240429063035_StatusAppointmentColumnMakeNullable")]
+    partial class StatusAppointmentColumnMakeNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- BackendProcessor/BackendProcessor/Models/Appointment.cs
+++ BackendProcessor/BackendProcessor/Models/Appointment.cs
@@ -24,9 +24,8 @@ namespace BackendProcessor.Models
         [Required]
         public DateTime AppointmentTime { get; set; }
 
-        [Required]
         [StringLength(50)]
-        public string Status { get; set; }
+        public string? Status { get; set; }
 
         [StringLength(500)]
         public string? Notes { get; set; }
