6bbd36d75eb3d2ab7e74cdfe9f1cc44a1ad9307a
superalex-dev <mr.naidobrixjr@gmail.com>
2024-05-03 16:16:29 +0300

    start coding the model

--- AIModel/AnalysingHealthcareAi/AnalysingHealthcareAi/AnalysingHealthcareAi.py
+++ AIModel/AnalysingHealthcareAi/AnalysingHealthcareAi/AnalysingHealthcareAi.py
@@ -1,1 +1,17 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
 
+load_dotenv()
+print(os.getenv("DATABASE_URL"))
+
+engine = create_engine(database_url)
+
+conn = engine.connect()
+
+df = pd.read_sql("SELECT Reason FROM Appointments", conn)
+
+print(df.head())
+
+conn.close()
