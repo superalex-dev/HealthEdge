6e8f2c7b06797b45e6c767d4ae619bb56e44cb5f
superalex-dev <mr.naidobrixjr@gmail.com>
2024-05-09 22:33:30 +0300

    upgrading design of the mail template

--- BackendProcessor/BackendProcessor/Services/EmailService.cs
+++ BackendProcessor/BackendProcessor/Services/EmailService.cs
@@ -19,9 +19,19 @@ namespace BackendProcessor.Services
             var from = new EmailAddress("mr.naidobrixjr@gmail.com", "Alexander Boev - HealthEdge CEO");
             var to = new EmailAddress(userEmail, userName);
             var subject = "Welcome to HealthEdge";
-            var message = $"Dear {userName},\n\nThank you for registering!";
-            var plainTextContent = message;
-            var htmlContent = $"<strong>{message}</strong>";
+            var plainTextContent = $"Dear {userName},\n\nThank you for registering! Please read our terms of service here: https://docs.google.com/document/d/1FrodI2JhR99iOWQzPlniHNKRHVYD20zALzqaopPsxxk/edit?usp=sharing";
+
+            var htmlContent = $@"
+                <html>
+                    <body>
+                        <h1>Welcome to HealthEdge, {userName}!</h1>
+                        <p>Thank you for registering. We're excited to have you on board.</p>
+                        <p>Please read our <a href='https://docs.google.com/document/d/1FrodI2JhR99iOWQzPlniHNKRHVYD20zALzqaopPsxxk/edit?usp=sharing'>terms of service</a>.</p>
+                        <p>Best regards,</p>
+                        <p>Alexander Boev - HealthEdge CEO</p>
+                    </body>
+                </html>";
+
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
 
             try
